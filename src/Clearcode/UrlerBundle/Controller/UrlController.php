f52dc7967d05712cedf730ee8a3e107ba3239bc9
diosmosis <benakamoorthi@fastmail.fm>
2014-08-01 21:47:59 +0200

    Adding PiwikTracker library + minimal test for UrlController.

--- src/Clearcode/UrlerBundle/Controller/UrlController.php
+++ src/Clearcode/UrlerBundle/Controller/UrlController.php
@@ -4,9 +4,13 @@ namespace Clearcode\UrlerBundle\Controller;
 
 use Clearcode\UrlerBundle\Exception\InvalidTokenException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Clearcode\UrlerBundle\Entity\Url;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Controller\Annotations\View;
+use PiwikTracker;
+
+require_once '../lib/PiwikTracker/PiwikTracker.php';
 
 class UrlController extends Controller
 {
@@ -20,12 +24,13 @@ class UrlController extends Controller
             throw $this->createNotFoundException('Link not found');
         }
 
+        $this->trackUrlVisit($link);
+
         $type = $link->getType();
 
         switch ($type) {
             case Url::TYPE_SIMPLE:
                 return $this->redirect($link->getUrl());
-                break;
             case Url::TYPE_MULTIPLE:
                 break;
             case Url::TYPE_PASSWORD:
@@ -55,5 +60,11 @@ class UrlController extends Controller
 
 
         return $urlObject;
+    }
+
+    private function trackUrlVisit($link)
+    {
+        $tracker = new PiwikTracker($idSite = 1); // TODO
+        // TODO
     }
 }
